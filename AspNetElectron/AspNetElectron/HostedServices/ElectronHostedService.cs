5713b3d50a8fbfa64d917c7f54981a1997502e64
melnikovmaxim <melnikovmaxim.nhk@gmail.com>
2022-10-12 16:59:25 +1000

    no message

--- AspNetElectron/AspNetElectron/HostedServices/ElectronHostedService.cs
+++ AspNetElectron/AspNetElectron/HostedServices/ElectronHostedService.cs
@@ -1,10 +1,20 @@
 ﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
 
 namespace AspNetElectron.HostedServices;
 
 public class ElectronHostedService: IHostedService
 {
-    public Task StartAsync(CancellationToken _) => Electron.WindowManager.CreateWindowAsync();
+    public Task StartAsync(CancellationToken _)
+    {
+        var windowsOptions = new BrowserWindowOptions()
+        {
+            Width = 920,
+            Height = 800
+        };
+        
+        return Electron.WindowManager.CreateWindowAsync(windowsOptions);
+    }
 
     public Task StopAsync(CancellationToken _)
     {
